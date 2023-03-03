e039e075ce76c2f53878a5814b94c0519471f205
Jip Irfandy <irfandyjip98@gmail.com>
2023-03-03 22:20:29 +0700

    feat: integration between service and typeorm

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User) // Used to help DI, because DI doesn't read generics well
+    private usersRepository: Repository<User>, 
+  ) {}
+
+  async create(email: string, password: string) {
+    const user = this.usersRepository.create({ username: email, password });
+    await this.usersRepository.save(user);
+    return user;
+  }
+}
