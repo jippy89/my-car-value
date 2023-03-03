fe492706cdc0c2e01c18a948d6b70d107a2693dd
Jip Irfandy <irfandyjip98@gmail.com>
2023-03-03 22:25:49 +0700

    feat: implement Sign Up feature

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,17 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
 
+  constructor (
+    private usersService: UsersService,
+  ) {}
+
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
-    console.log(body)
-    return 'This action adds a new user';
+    return this.usersService.create(body.email, body.password);
   }
   
 }
