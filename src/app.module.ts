46001dff8e3f0ec6a46382094ca0195aea792916
Jip Irfandy <irfandyjip98@gmail.com>
2023-03-03 21:40:17 +0700

    feat: create `report` entity and integrate with `AppModule`

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,12 +7,13 @@ import { ReportsModule } from './reports/reports.module';
 
 // Entities
 import { User } from './users/user.entity';
+import { Report } from './reports/report.entity';
 
 @Module({
   imports: [TypeOrmModule.forRoot({
     type: 'sqlite',
     database: 'db.sqlite',
-    entities: [User],
+    entities: [User, Report],
     synchronize: true,
   }), UsersModule, ReportsModule],
   controllers: [AppController],
