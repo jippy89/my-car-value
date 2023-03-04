67a7524b9c2ea33acbc70383d0070cdbabd47a1c
Jip Irfandy <irfandyjip98@gmail.com>
2023-03-04 15:47:15 +0700

    refactor: add interceptor to every route

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Get, Query, Delete, Param, Patch, NotFoundException, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Post, Get, Query, Delete, Param, Patch, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
@@ -6,6 +6,7 @@ import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
 
   constructor (
@@ -17,7 +18,6 @@ export class UsersController {
     return this.usersService.create(body.email, body.password);
   }
 
-  @Serialize(UserDto)
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     console.log('handler is running')
