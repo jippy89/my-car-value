9cb7e81e5cf2be113111940cecfd20ba569a0370
Jip Irfandy <irfandyjip98@gmail.com>
2023-03-04 16:01:39 +0700

    feat: generate `authentication` service inside `users` directory

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { AuthenticationService } from './authentication.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService, AuthenticationService]
 })
 export class UsersModule {}
