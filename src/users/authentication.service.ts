c716ddcb325cee8b59291cc5a28953e96832c926
Jip Irfandy <irfandyjip98@gmail.com>
2023-03-04 18:18:16 +0700

    feat(services): add `signup` method to authentication service

--- src/users/authentication.service.ts
+++ src/users/authentication.service.ts
@@ -1,11 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
+
+const scrypt = promisify(_scrypt)
 
 @Injectable()
 export class AuthenticationService {
   constructor(private userService: UsersService) {}
 
-  signup(email: string, password: string) {}
+  async signup(email: string, password: string) {
+    // See if email is in use
+    const users = await this.userService.find(email)
+    if (users.length) {
+      throw new BadRequestException('Email in use')
+    }
+
+    // Generate a salt for password
+    const salt = randomBytes(8).toString('hex')
+    
+    // Hash the password
+    const hash = (await scrypt(password, salt, 32)) as Buffer
+    const hashedPassword = `${salt}.${hash.toString('hex')}`
+
+    // Create a new user and save
+    const user = await this.userService.create(email, hashedPassword)
+
+    // Return user
+    return user
+  }
 
   signin() {}
 }
