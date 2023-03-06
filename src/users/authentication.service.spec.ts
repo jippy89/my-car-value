837430e45d136da29c372f479b68f3b90a2b9431
Jip Irfandy <irfandyjip98@gmail.com>
2023-03-06 17:04:42 +0700

    test(users): recreate `AuthenticationService` in test environment

--- src/users/authentication.service.spec.ts
+++ src/users/authentication.service.spec.ts
@@ -1,18 +1,43 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthenticationService } from './authentication.service';
+import { UsersService } from './users.service';
 
-describe('AuthenticationService', () => {
-  let service: AuthenticationService;
+it('can create an instance of users service', async () => {
+  const fakeUsersService = {
+    find: () => Promise.resolve([]),
+    findOne: () => Promise.resolve({}),
+    create: (email: string, password: string) => Promise.resolve({ id: 1, email, password }),
+  }
+  
+  // Create testing DI Container
+  const module = await Test.createTestingModule({
+    providers: [
+      AuthenticationService,
+      // Basically, telling Nest DI Container to use fakeUsersService
+      // when it look for UsersService
+      {
+        provide: UsersService,
+        useValue: fakeUsersService,
+      }
+    ],
+  }).compile()
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthenticationService],
-    }).compile();
-
-    service = module.get<AuthenticationService>(AuthenticationService);
-  });
-
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+  const service = module.get(AuthenticationService)
+  expect(service).toBeDefined()
 });
+
+// describe('AuthenticationService', () => {
+//   let service: AuthenticationService;
+
+//   beforeEach(async () => {
+//     const module: TestingModule = await Test.createTestingModule({
+//       providers: [AuthenticationService],
+//     }).compile();
+
+//     service = module.get<AuthenticationService>(AuthenticationService);
+//   });
+
+//   it('should be defined', () => {
+//     expect(service).toBeDefined();
+//   });
+// });
