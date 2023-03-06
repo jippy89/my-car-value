64b7a9571459c2bbf36cac402d6cd6444aff097e
Jip Irfandy <irfandyjip98@gmail.com>
2023-03-06 17:15:31 +0700

    refactor(test): refactor so fresh `AuthenticationService` instance is made for every test

--- src/users/authentication.service.spec.ts
+++ src/users/authentication.service.spec.ts
@@ -2,48 +2,39 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AuthenticationService } from './authentication.service';
 import { UsersService } from './users.service';
 
-it('can create an instance of users service', async () => {
-  const fakeUsersService: Partial<UsersService> = {
-    find: () => Promise.resolve([]),
-    // Notes: In the course, they modify the user entity
-    // And it makes an error because they didn't implement `logUser`, etc
-    // But you didn't got it because you don't want to modify the user entity
-    // So they create this solution instead
-    // create: (email: string, password: string) =>
-    //   Promise.resolve({ id: 'asdfas', username: email, password } as User),
-    create: (email: string, password: string) =>
-      Promise.resolve({ id: 'asdfas', username: email, password }),
-  }
-  
-  // Create testing DI Container
-  const module = await Test.createTestingModule({
-    providers: [
-      AuthenticationService,
-      // Basically, telling Nest DI Container to use fakeUsersService
-      // when it look for UsersService
-      {
-        provide: UsersService,
-        useValue: fakeUsersService,
-      }
-    ],
-  }).compile()
+describe('AuthenticationService', () => {
+  let service: AuthenticationService;
 
-  const service = module.get(AuthenticationService)
-  expect(service).toBeDefined()
+  beforeEach(async () => {
+    const fakeUsersService: Partial<UsersService> = {
+      find: () => Promise.resolve([]),
+      // Notes: In the course, they modify the user entity
+      // And it makes an error because they didn't implement `logUser`, etc
+      // But you didn't got it because you don't want to modify the user entity
+      // So they create this solution instead
+      // create: (email: string, password: string) =>
+      //   Promise.resolve({ id: 'asdfas', username: email, password } as User),
+      create: (email: string, password: string) =>
+        Promise.resolve({ id: 'asdfas', username: email, password }),
+    }
+
+    // Create testing DI Container
+    const module = await Test.createTestingModule({
+      providers: [
+        AuthenticationService,
+        // Basically, telling Nest DI Container to use fakeUsersService
+        // when it look for UsersService
+        {
+          provide: UsersService,
+          useValue: fakeUsersService,
+        }
+      ],
+    }).compile()
+
+    service = module.get(AuthenticationService)
+  });
+
+  it('can create an instance of users service', async () => {
+    expect(service).toBeDefined()
+  });
 });
-
-// describe('AuthenticationService', () => {
-//   let service: AuthenticationService;
-
-//   beforeEach(async () => {
-//     const module: TestingModule = await Test.createTestingModule({
-//       providers: [AuthenticationService],
-//     }).compile();
-
-//     service = module.get<AuthenticationService>(AuthenticationService);
-//   });
-
-//   it('should be defined', () => {
-//     expect(service).toBeDefined();
-//   });
-// });
