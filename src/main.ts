146c15df1d108a8186a35b540f1d120e9bf9c923
Jip Irfandy <irfandyjip98@gmail.com>
2023-03-07 14:44:36 +0700

    refactor: move the setup of global pipe to `app.module.ts`

--- src/main.ts
+++ src/main.ts
@@ -9,9 +9,6 @@ async function bootstrap() {
   app.use(cookieSession({
     keys: ['asdfasdf'],
   }))
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-  }));
   await app.listen(3000);
 }
 bootstrap();
