5c216c4ce489377b5e27deb067c063494001042a
Jip Irfandy <irfandyjip98@gmail.com>
2023-03-07 14:10:30 +0700

    test(auth): add e2e testing files for `auth`
    Doesn't work thought

---
+++ test/auth.e2e-spec.ts
@@ -0,0 +1,36 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+
+describe('Authentication System', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('handles signup request', () => {
+    const user = {
+      email: 'abc@abc.com',
+      password: 'abc',
+    }
+    return request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({
+        email: user.email,
+        password: user.password,
+      })
+      .expect(201)
+      .then((res) => {
+        const { id, email } = res.body;
+        expect(id).toBeDefined();
+        expect(email).toEqual(user.email);
+      })
+  });
+});
