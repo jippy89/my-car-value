161c0ff7aa5da14b505f06fa142157daf2ed8527
Jip Irfandy <irfandyjip98@gmail.com>
2023-03-08 18:03:54 +0700

    feat(reports): add ability to POST reports

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { AuthenticationGuard } from 'src/guards/authentication.guard';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { ReportsService } from './reports.service';
 
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+  constructor(private reportsService: ReportsService) {}
+
+  @Post()
+  @UseGuards(AuthenticationGuard)
+  createReport (@Body() body: CreateReportDto) {
+    return this.reportsService.create(body);
+  }
+}
