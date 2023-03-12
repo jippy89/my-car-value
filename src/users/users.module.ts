570fd3d65dc41fb131809006ac5063db485db0ff
Jip Irfandy <irfandyjip98@gmail.com>
2023-03-12 11:03:11 +0700

    refactor(users): remove the use of `CurrentUserInterceptor`
    This is solution to early commit's problem. It's because Interceptor runs AFTER the Guard, so we can't check if `currentUser` exist.

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,22 +1,20 @@
-import { Module } from '@nestjs/common';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { AuthenticationService } from './authentication.service';
-import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
+import { CurrentUserMiddleware } from './middlewares/current-user.middleware';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService, AuthenticationService,
-    // Sets global interceptor
-    // Affects all controler even other modules
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: CurrentUserInterceptor,
-    }
-  ]
+  providers: [UsersService, AuthenticationService]
 })
-export class UsersModule {}
+export class UsersModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(CurrentUserMiddleware)
+      .forRoutes('*');
+  }
+}
