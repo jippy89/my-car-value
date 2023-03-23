c0b83e2672a467d9f9eac81826c42724426f325f
Jip Irfandy <irfandyjip98@gmail.com>
2023-03-24 05:17:31 +0700

    style(db): commented this because turns out we dont need it

--- src/db/factories/user.factory.ts
+++ src/db/factories/user.factory.ts
@@ -1,11 +1,11 @@
-import { setSeederFactory } from 'typeorm-extension';
-import { User } from '../../users/user.entity';
+// import { setSeederFactory } from 'typeorm-extension';
+// import { User } from '../../users/user.entity';
 
-export default setSeederFactory(User, (faker) => {
-    const user = new User();
-    user.username = faker.internet.email();
-    // Roles can be null, or admin, or user and can be multiple all in one string separated by commas
-    user.roles = faker.helpers.arrayElement([null, 'admin', 'user', 'admin,user']);
-    user.password = faker.internet.password();
-    return user;
-})
+// export default setSeederFactory(User, (faker) => {
+//     const user = new User();
+//     user.username = faker.internet.email();
+//     // Roles can be null, or admin, or user and can be multiple all in one string separated by commas
+//     user.roles = faker.helpers.arrayElement([null, 'admin', 'user', 'admin,user']);
+//     user.password = faker.internet.password();
+//     return user;
+// })
