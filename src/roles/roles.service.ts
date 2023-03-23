01ee5cfa71d553920b30e3715323ef60712c64b0
Jip Irfandy <irfandyjip98@gmail.com>
2023-03-24 05:27:09 +0700

    feat(roles): add `findOneByName` to roles service

--- src/roles/roles.service.ts
+++ src/roles/roles.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Role } from './role.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class RolesService {
+  constructor (
+    @InjectRepository(Role)
+    private rolesRepository: Repository<Role>,
+  ) {}
+
   create(createRoleDto: CreateRoleDto) {
     return 'This action adds a new role';
   }
@@ -14,6 +22,11 @@ export class RolesService {
 
   findOne(id: number) {
     return `This action returns a #${id} role`;
+  }
+
+  async findOneByName(name: string) {
+    // return this.rolesRepository.findOne
+    return this.rolesRepository.findOneBy({ name });
   }
 
   update(id: number, updateRoleDto: UpdateRoleDto) {
