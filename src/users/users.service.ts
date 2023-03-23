8a06e2a4c1b5c63421847ae0bb82e9ae448f05c6
Jip Irfandy <irfandyjip98@gmail.com>
2023-03-24 05:30:23 +0700

    feat(users): add ability to set default role

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,16 +2,23 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
+import { RolesService } from 'src/roles/roles.service';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User) // Used to help DI, because DI doesn't read generics well
-    private usersRepository: Repository<User>, 
+    private usersRepository: Repository<User>,
+    private rolesService: RolesService,
   ) {}
 
   async create(email: string, password: string) {
-    const user = this.usersRepository.create({ username: email, password });
+    const defaultRole = await this.rolesService.findOneByName('user');
+    const user = this.usersRepository.create({
+      username: email,
+      password,
+      roles: [defaultRole],
+    });
     await this.usersRepository.save(user);
     return user;
   }
