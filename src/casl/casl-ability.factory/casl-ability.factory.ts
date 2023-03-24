9704da54809c635e8cb5ca4088a7011499384f2f
Jip Irfandy <irfandyjip98@gmail.com>
2023-03-24 15:17:09 +0700

    feat(enums): add actions towards things that can be done to entities
    refactor(casl): move `actions` to its own directory
    
    refactor(casl): rename `reports.action.ts` to `actions.enum.ts`
    
    refactor(enums): move `actions.enum.ts` to src directory
    
    feat(enums): add new action

---
+++ src/casl/casl-ability.factory/casl-ability.factory.ts
@@ -0,0 +1,42 @@
+import { Ability, AbilityBuilder, AbilityClass, ExtractSubjectType, InferSubjects, defineAbility } from "@casl/ability";
+import { Injectable } from "@nestjs/common";
+import { Action } from "src/enums/actions.enum";
+import { Report } from "src/reports/report.entity";
+import { User } from "src/users/user.entity";
+
+type Subjects = InferSubjects<typeof Report | typeof User> | 'all';
+
+export type AppAbility = Ability<[Action, Subjects]>;
+
+@Injectable()
+export class CaslAbilityFactory {
+
+  createForUser(user: User) {
+    const { can, cannot, build } = new AbilityBuilder<AppAbility>
+      (Ability as AbilityClass<AppAbility>);
+
+    if (!user.roles) can(Action.Read, 'all')
+
+    // Permissions for all resources
+    if (Array.isArray(user.roles)) {
+      const roles = user.roles.filter(role => role.name === Role.Admin)
+      if (roles.length > 0) {
+        can(Action.Manage, 'all')
+      }
+    } else if (typeof user.roles === 'object') {
+      if (user.roles.admin) {
+        can(Action.Manage, 'all')
+      }
+    }
+
+    // Permissions for Report resource
+    can(Action.Update, Report, { user: { id: user.id } })
+    cannot(Action.Delete, Report, { approved: true })
+
+    return build({
+      detectSubjectType: item =>
+        item.constructor as ExtractSubjectType<Subjects>
+    })
+  }
+
+}
